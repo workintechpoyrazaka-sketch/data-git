8cac7ae42e73592ac6464063852fdddfa15bde2e
Adil Poyraz AKA <workintech.poyrazaka@gmail.com>
2026-06-03 14:59:10 +0300

    Add docstrings and fix style, pylint now 10/10

--- today.py
+++ today.py
@@ -4,4 +4,10 @@ This is the module docstring
 
 
 def my_name_is():
+    """Return my GitHub username."""
+    return "workintechpoyrazaka-sketch"
+
+
+def my_buddy_is():
+    """Return my buddy's GitHub username."""
     return "workintechpoyrazaka-sketch"
